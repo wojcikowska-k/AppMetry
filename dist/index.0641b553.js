13be2516665ab75bf08a7b6f7bb69712867fd11f
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-02-27 11:08:24 +0100

    changed js

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -563,14 +563,14 @@ const veneerThickEl = document.getElementById("veneer-thick");
 const totalOutputEl = document.getElementById("total-output");
 // ### EVENTS
 calculateBtnEl.addEventListener("click", ()=>{
-    totalOutputEl.innerHTML = calculate(veneerThickEl.value, rollThickEl.value);
+    totalOutputEl.innerHTML = calculate(Number(veneerThickEl.value), Number(rollThickEl.value));
     rollThickEl.value = "";
 });
 // ### CALCULATE
 const calculate = (veneerThick, rollThick)=>{
-    const INSIDE_ROLL_RADIUS = 50;
+    const INSIDE_ROLL_RADIUS = 100;
     let total = 0;
-    for(i = INSIDE_ROLL_RADIUS; i <= rollThick; i += veneerThick)total = total + 2 * Math.PI * i;
+    for(i = INSIDE_ROLL_RADIUS; i <= INSIDE_ROLL_RADIUS + rollThick; i += veneerThick)total = total + 2 * Math.PI * i;
     return `${Math.round(total) / 10} cm`;
 };
 
